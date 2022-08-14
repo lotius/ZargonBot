b97ac399c8b17057b17587b601b87e059834371c
Rich Kee <haderach81@gmail.com>
2022-08-14 17:20:48 -0500

    Initial commit.

---
+++ commands.py
@@ -0,0 +1,83 @@
+import random
+import os
+import cv2
+import numpy as np
+import discord
+
+async def process_command(message, command, param):
+    if (command == 'commands'):
+        await commands(message)
+    elif (command == 'roll'):
+        await roll(message, param)
+    elif (command == 'hqroll'):
+        await heroquest_roll(message, param)
+    elif (command == 'terry'):
+        await terry(message)
+    elif (command == 'disappointed'):
+        await disappointed(message)
+
+def can_convert_to_int(string):
+    try:
+        int(string)
+
+        return True
+    except ValueError:
+        return False
+
+async def commands(message):
+    await message.channel.send(f'Command List:\n \
+            !roll - Roll a specified set of dice.\n \
+            !hqroll - Roll HeroQuest combat dice.\n \
+            !terry - Post a random Terry A. Davis picture.\n \
+            !disappointed - Kevin Sorbo is very disappointed.')
+
+async def roll(message, param):
+    dice = param.split('d', 1)
+    diceTotalDetail = []
+
+    if (len(dice) != 2 or not can_convert_to_int(dice[0]) or not can_convert_to_int(dice[1])):
+        await message.channel.send('Proper roll format is #d#! (Example: 2d6)')
+        return
+
+    for x in range(int(dice[0])):
+        diceTotalDetail.append(random.randint(1, int(dice[1])))
+
+    await message.channel.send(f'**{message.author.name}** rolled **{sum(diceTotalDetail)}** _{diceTotalDetail}_.')
+
+async def heroquest_roll(message, param):
+    image = cv2.imread('images/hqdice/dicefaces.png', cv2.IMREAD_COLOR)
+    skull = image[0 : 100, 0 : 110] # y1:y2, x1:x2
+    whiteshield = image[0 : 100, 110 : 220]
+    blackshield = image[0 : 100, 220 : 330]
+    diceRolls = []
+
+    if (can_convert_to_int(param) and int(param) > 0 and int(param) <= 15):
+        for x in range(int(param)):
+            diceRolls.append(random.randint(1, 6))
+    else:
+        await message.channel.send('HeroQuest combat roll command usage: !hqroll # (Example: !hqroll 3). Max roll of 15.')
+        return
+    
+    result_image = np.zeros((100, len(diceRolls) * 110, 3), np.uint8)
+    for x in range(len(diceRolls)):
+        if (diceRolls[x] == 1 or diceRolls[x] == 2 or diceRolls[x] == 3):
+            result_image[0 : 100, 110 * x : 110 * (x + 1)] = skull
+        elif (diceRolls[x] == 4 or diceRolls[x] == 5):
+            result_image[0 : 100, 110 * x : 110 * (x + 1)] = whiteshield
+        elif (diceRolls[x] == 6):
+            result_image[0 : 100, 110 * x : 110 * (x + 1)] = blackshield
+    
+    cv2.imwrite('images/hqdice/results.png', result_image)
+
+    await message.channel.send(file=discord.File('images/hqdice/results.png'))
+
+async def terry(message):
+    path = 'images/terry'
+    files = os.listdir(path)
+
+    file_to_send = files[random.randint(0, len(files) - 1)]
+    
+    await message.channel.send(file=discord.File(f'images/terry/{file_to_send}'))
+
+async def disappointed(message):
+    await message.channel.send(file=discord.File(f'images/disappointed.gif'))
---
+++ gamebot.py
@@ -0,0 +1,59 @@
+# bot.py
+import os
+import commands
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+intents = discord.Intents.default()
+intents.members = True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+    # guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
+    guild = discord.utils.get(client.guilds, name=GUILD)
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+    members = ""
+    for member in guild.members:
+        members += "\n - " + member.name + " (" + str(member.id) + ")"
+
+    #members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+
+@client.event
+async def on_message(message):
+    command = ""
+    params = ""
+
+    if (message.channel.id == os.getenv('CHANNEL_ID') and message.author.id != os.getenv('BOT_ID') and message.author.id != os.getenv('MEE6_ID') and not message.attachments): 
+        if (message.content[0] == '!' and len(message.content) > 1):
+
+            space_position = message.content.find(' ')
+            
+            if (space_position != -1):
+                command = message.content[1:space_position]
+                params = message.content[space_position + 1:]
+            else:
+                command = message.content[1:]
+
+            print(f'Command entered: {command}')
+            print(f'Parameters entered: {params}')
+
+            await commands.process_command(message, command, params)
+    else:
+        return
+
+client.run(TOKEN)
+
