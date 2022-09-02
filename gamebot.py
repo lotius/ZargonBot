15dbaa12407f2dd94c079d659a0067d40f73d4fa
Rich Kee <kee@clackcorp.com>
2022-09-02 10:34:41 -0500

    Changed to Ribby channel ID instead of name.

--- gamebot.py
+++ gamebot.py
@@ -37,7 +37,9 @@ async def on_message(message):
     command = ""
     params = ""
 
-    if (message.channel.name == 'ribbys-hall-of-wonders' and message.author.bot == False and not message.attachments and not message.is_system()): 
+    print(f'Content: {message.content}')
+    
+    if (message.channel.name == int(os.getenv('RIBBY_CHANNEL_ID')) and message.author.bot == False and not message.attachments and not message.is_system()): 
         if (message.content[0] == '!' and len(message.content) > 1):
 
             space_position = message.content.find(' ')
