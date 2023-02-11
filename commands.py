8d09b2cab0794b3f2c9e8e435afbd86af943907b
Rich Kee <haderach81@gmail.com>
2023-02-11 16:46:35 -0600

    Added DOOM combat dice.

--- commands.py
+++ commands.py
@@ -6,16 +6,19 @@ from discord.ui import Select, View
 from discord.ext import commands
 from hqdice import checkHeroQuestCombatDiceParameters
 from scdice import checkSpaceCrusadeCombatDiceParameters
+from doomdice import checkDoomCombatDiceParameters
 
 async def process_command(message, command, param):
     if (command == 'help'):
         await help(message, param)
-    elif (command == 'roll'):
+    elif (command == 'roll' or command == 'dice'):
         await roll(message, param)
-    elif (command == 'hqroll'):
+    elif (command == 'hqroll' or command == 'hqdice'):
         await heroquest_roll(message, param)
-    elif (command == 'scroll' or command == 'sqroll'):
+    elif (command == 'scroll' or command == 'sqroll' or command == 'scdice' or command == 'sqdice'):
         await spacecrusade_roll(message, param)
+    elif (command == 'doomroll' or command == 'doomdice'):
+        await doom_roll(message, param)
 
 def can_convert_to_int(string):
     try:
@@ -32,6 +35,8 @@ async def help(message, param):
 !hqroll - Roll the HeroQuest combat dice. For example, if you\'d like to roll 3 combat dice type: !hqroll 3\n \
 !scroll or !sqroll - Roll the Space Crusade combat dice. For example, if you\'d like to roll 2 white \
 combat dice type: !scroll 2 white\n \
+!doomdice - Roll the DOOM 2016 board game combat dice. For example, if you\'d like to roll 2 red \
+combat dice type: !doomdice 2 red\n \
 Use !help _command_ to get more specific information about an available command.')
     elif (param == 'roll'):
         await message.channel.send(f'**Roll standard dice**:\n \
@@ -52,6 +57,12 @@ To roll Space Crusade dice use the _**!scroll**_ or _**!sqroll**_ command follow
 number of dice you wish to roll, followed by the color you wish to roll. In Space Crusade \
 you can roll up to 4 white and up to 4 red dice.\n \
 **Examples:** _**!scroll 2 white**, **!scroll 1 red 3 white**, **!sqroll 1 white 2 red**_')
+    elif (param == 'doomroll' or param == 'doomdice'):
+        await message.channel.send(f'**Roll DOOM (2016) combat dice**:\n \
+To roll DOOM dice use the _**!doomroll**_ or _**!doomdice**_ command followed by the \
+number of dice you wish to roll, followed by the color you wish to roll. In the Doom 2016 \
+board game you can roll up to 4 red and up to 2 black dice.\n \
+**Examples:** _**!doomdice 2 red**, **!doomdice 2 red 2 black**, **!doomdice 1 red 3 black**_')
 
 async def roll(message, param):
     dice = param.split('d', 1)
@@ -80,7 +91,6 @@ to review the proper usage of the _hqroll_ command.')
         return
 
 async def spacecrusade_roll(message, param):
-    print('Entered space crusade function.')
     # Determine if regex was matched. A combination of a digit followed by a
     # word can be matched up to 2 times as there are only red and white dice in Space Crusade.
     regex = re.compile(r'^(\d{1}\s[a-zA-Z]+\b\s?){1,2}$')
@@ -91,4 +101,17 @@ async def spacecrusade_roll(message, param):
     else:
         await message.channel.send(f'**{message.author.name}** your input pattern is invalid! Please use _!help scroll_ \
 to review the proper usage of the _scroll_ command.')
+        return
+    
+async def doom_roll(message, param):
+    # Determine if regex was matched. A combination of a digit followed by a
+    # word can be matched up to 2 times as there are only red and black dice in DOOM.
+    regex = re.compile(r'^(\d{1}\s[a-zA-Z]+\b\s?){1,2}$')
+
+    match = re.match(regex, param)
+    if match:
+        await checkDoomCombatDiceParameters(message, param)
+    else:
+        await message.channel.send(f'**{message.author.name}** your input pattern is invalid! Please use _!help doomdice_ \
+to review the proper usage of the _doomdice_ command.')
         return
---
+++ doomdice.py
@@ -0,0 +1,89 @@
+import random
+import numpy as np
+import cv2
+import discord
+
+async def checkDoomCombatDiceParameters(message, param):
+    params = param.split(' ')
+    totalDiceRolled = 0
+    diceToRoll = []
+    
+    paramArray = np.array_split(params, len(params) / 2)
+
+    # Iterate the sets of dice quantities and requested colors.
+    for currentParam in paramArray:
+        numToRoll = int(currentParam[0])
+        currentColor = currentParam[1]
+    
+        totalDiceRolled = totalDiceRolled + numToRoll
+        # Ensure both the current numToRoll and the cumulative total dice rolled do not exceed 6, and
+        # red is only rolled up to 4 times, and black only rolled up to 2 times.
+        if (totalDiceRolled > 6 or (currentColor == 'red' and numToRoll > 4) or (currentColor == 'black' and numToRoll > 2)):
+            await message.channel.send('Sorry, but you cannot roll more than 4 of red dice, more than 2 black die, \
+and/or up to 6 total combat dice in a single command.')
+            return
+        
+        # Ensure that at least 1 die of each requested color is rolled.
+        if (numToRoll < 1):
+            await message.channel.send('Sorry, but for each die color specified you must roll at least 1 die.')
+            return
+        
+        # Ensure that the colors requested are available.
+        if (currentColor != 'red' and currentColor != 'black'):
+            await message.channel.send('Sorry, but you\'ve included a die color that isn\'t available. Available colors \
+are red and/or black.')
+            return
+        
+        for existingDice in diceToRoll:
+            for dieColor in existingDice.values():
+                if (currentColor == dieColor):
+                    await message.channel.send('Sorry, but each die color can only be specified once.')
+                    return
+
+        diceToRoll.append({'face': currentColor, 'numToRoll': int(numToRoll)})
+
+    await rollDoomCombatDice(message, diceToRoll)
+    
+# Function takes 3 parameters:
+# 1. An array describing the dice faces of a die to be rolled and how many of each face are on that die.
+# Example: [{'face': 'red-blank.png', 'numOfFaces': 1}, {'face': 'red-1shot.png', 'numOfFaces': 3}, {'face': 'red-2shots.png', 'numOfFaces': 2}]
+# 2. The number of dice to be rolled.
+async def rollDoomCombatDice(message, diceToRoll):
+    red = [{'face': 'red-blank.png', 'numOfFaces': 1}, {'face': 'red-1shot.png', 'numOfFaces': 3}, {'face': 'red-2shots.png', 'numOfFaces': 2}]
+    black = [{'face': 'black-1shot.png', 'numOfFaces': 2}, {'face': 'black-2shots', 'numOfFaces': 3}, {'face': 'black-3shots', 'numOfFaces': 1}]
+    diceFaceCount = 0
+    diceImages = {}
+    rolledDice = []
+
+    # Assemble current dice's faces
+    for currentRequestedFace in diceToRoll:
+
+        # Color of current face
+        currentFaceColor = currentRequestedFace['face']
+        if (currentFaceColor == 'red'):
+            currentFace = red
+        elif (currentFaceColor == 'black'):
+            currentFace = black
+        
+        # Assemble the current color's dice faces
+        for coloredFace in currentFace:
+            for x in range(0, coloredFace['numOfFaces']):
+                diceFaceCount = diceFaceCount + 1
+                diceImages[diceFaceCount] = cv2.imread('images/doom/' + coloredFace['face'], cv2.IMREAD_COLOR)
+
+        # Roll the dice and save the appropriate face to an array
+        for x in range(int(currentRequestedFace['numToRoll'])):
+            rolledDice.append(diceImages[random.randint(1, 6)])
+                
+        diceFaceCount = 0
+        diceImages.clear()
+
+    result_image = np.zeros((100, len(rolledDice) * 108, 3), np.uint8)
+    x = 0
+    for currentDiceFace in rolledDice:
+        result_image[0 : 100, 108 * x : 108 * (x + 1)] = currentDiceFace
+        x = x + 1
+
+    cv2.imwrite('images/doom/results.png', result_image)
+
+    await message.channel.send(file=discord.File('images/doom/results.png'))
